1bff8c46c6218edad85d326fb43a4d0ae88eb76b
Daniel Cheng and Patrick Napper <patrick@>
2017-10-24 10:12:59 -0700

    Update Homecontroller and Views to show Homepage, AddCategory, and ViewCategories

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace TodoList.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet("/category/add")]
+    public ActionResult AddCategory()
+    {
+      return View();
+    }
+
+    [HttpPost("/category/list")]
+    public ActionResult ViewCategories()
+    {
+      Category newCategory = new Category(Request.Form["category-name"]);
+      newCategory.Save();
+      List<Category> allCategories = Category.GetAll();
+
+      return View(allCategories);
+    }
+
+    [HttpGet("/{name}/tasklist")]
+    public ActionResult CategoryDetail(string name)
+    {
+
+    }
+  }
+}
